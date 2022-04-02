f10ed2a6497351b823975aa5904e81c0d84cccc8
Adam Snihur <adam.snihur@gmail.com>
2022-04-02 14:58:50 +0200

    odejmowanie zaimplementowane

---
+++ README.md
@@ -0,0 +1,5 @@
+Autor: Adam Snihur
+
+
+To jest repozytorium z kalkulatorem z inżynierii oprogramowania.
+znajdują się tutaj pierwsze próby dotyczące commitów i branchy a także funkcje tworzące kalkulator.
---
+++ test.py
@@ -0,0 +1,17 @@
+def hello(name):
+return "Hello" + str(name)
+
+def dodaj(a,b):
+wynik = float(a) + float(b)
+return wynik
+
+pierwsza = input()
+druga = input()
+
+
+def odejmij(a,b):
+return a-b
+
+
+
+print (dodaj(pierwsza, druga))
