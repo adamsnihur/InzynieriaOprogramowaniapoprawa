5caa7725c5799c124412537b422cebf5778d9a92
Adam Snihur <adam.snihur@gmail.com>
2022-04-21 23:20:24 +0200

    reverted changes in functions to include floats instead of ints

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ def hello(name):
 
 def odejmij(a,b):
 	wynik = float(a) - float(b)
-	return a-b
+	return wynik
 
 def dodaj(a,b):
 	wynik = float(a) + float(b)
 	return wynik
 
-pierwsza = int(input())
-druga = int(input())
+pierwsza = input()
+druga = input()
 
 print (dodaj(pierwsza,druga))
 
